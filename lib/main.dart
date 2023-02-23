4cbfa02bed352616287b95b7852079103928c876
riommx <Sid>
2023-02-23 12:16:13 -0300

    casa 23

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'app/app_module.dart';
 import 'app/app_widget.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   return runApp(
     ModularApp(
       module: AppModule(),
